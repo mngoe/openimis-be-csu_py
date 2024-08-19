fb7de3cd8940677429ace7e3f49eaa6f95f6537e
kevel <keveltiah@gmail.com>
2024-08-19 18:51:38 +0100

    feat: Script de signal en commentaire du a l'URL qui ne passe plus

--- csu/signals.py
+++ csu/signals.py
@@ -14,43 +14,43 @@ def bind_service_signals():
 def _on_add_policy(*args, **kwargs):
     data = kwargs.get('result', None)
     print("The policy ", data)
-    if data:
-        familyid = data.family_id
-        policies = Policy.objects.filter(family_id=familyid).exclude(id=data.id)
-        print("All policies ", policies)
-        if not policies:
-            # This is the first policy the famiily just had, lets send enrollement to camerhealth
-            # Note: One insuree equals to One familly
-            url = "http://3.137.55.229:8006/v1/auth/token/"
+    # if data:
+    #     familyid = data.family_id
+    #     policies = Policy.objects.filter(family_id=familyid).exclude(id=data.id)
+    #     print("All policies ", policies)
+    #     if not policies:
+    #         # This is the first policy the famiily just had, lets send enrollement to camerhealth
+    #         # Note: One insuree equals to One familly
+    #         url = "http://3.137.55.229:8006/v1/auth/token/"
 
-            payload = json.dumps({
-                "email": "openimis@minsante.cm ",
-                "password": "OpenImis1*"
-            })
-            headers = {
-            'Content-Type': 'application/json'
-            }
+    #         payload = json.dumps({
+    #             "email": "openimis@minsante.cm ",
+    #             "password": "OpenImis1*"
+    #         })
+    #         headers = {
+    #         'Content-Type': 'application/json'
+    #         }
 
-            response = requests.request("POST", url, headers=headers, data=payload)
+    #         response = requests.request("POST", url, headers=headers, data=payload)
 
-            response_data = response.json()
-            print("Response ", response_data)
+    #         response_data = response.json()
+    #         print("Response ", response_data)
 
-            token = response_data.get('access', False)
-            print("token ", token)
+    #         token = response_data.get('access', False)
+    #         print("token ", token)
 
-            if token:
-                insurees = Insuree.objects.filter(family=familyid)
-                print("insurees ", insurees)
-                for insuree in insurees:
+    #         if token:
+    #             insurees = Insuree.objects.filter(family=familyid)
+    #             print("insurees ", insurees)
+    #             for insuree in insurees:
 
-                    url = "http://3.137.55.229:8006/v1/enrollments/open_imis_enrol/?mpi="+str(insuree.chf_id)
+    #                 url = "http://3.137.55.229:8006/v1/enrollments/open_imis_enrol/?mpi="+str(insuree.chf_id)
 
-                    payload={}
-                    headers = {
-                    'Authorization': 'Bearer ' + str(token)
-                    }
+    #                 payload={}
+    #                 headers = {
+    #                 'Authorization': 'Bearer ' + str(token)
+    #                 }
 
-                    response = requests.request("GET", url, headers=headers, data=payload)
+    #                 response = requests.request("GET", url, headers=headers, data=payload)
 
-                    print(response.json())
+    #                 print(response.json())
